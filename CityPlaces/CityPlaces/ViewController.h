a73fbb57d0df005ebd5a6e6ef8dfc4a5f24ad31d
melanu1991 <melanu284733@gmail.com>
2017-08-11 10:34:17 +0300

    Create project

---
+++ CityPlaces/CityPlaces/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CityPlaces
+//
+//  Created by melanu1991 on 11.08.17.
+//  Copyright © 2017 melanu1991. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CityPlaces/CityPlaces/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CityPlaces
+//
+//  Created by melanu1991 on 11.08.17.
+//  Copyright © 2017 melanu1991. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
